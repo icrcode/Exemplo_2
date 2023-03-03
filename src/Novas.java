ea676daa7709a4f87b634e5bb37164db227ab273
Ícaro <icarobotelhosocial@gmail.com>
2023-03-03 19:00:46 -0300

    Exemplos da Aula 1 de ADS

---
+++ src/Novas.java
@@ -0,0 +1,333 @@
+public class Novas {
+    public static void main(String[] args) {
+        char[][] arrI = letraI();
+        escrever(arrI);
+
+        char[][] arrC = letraC();
+        escrever(arrC);
+
+        char[][] arrA = letraA();
+        escrever(arrA);
+
+        char[][] arrR = letraR();
+        escrever(arrR);
+
+        char[][] arrO = letraO();
+        escrever(arrO);
+
+    }
+
+    public static char[][] letraA() {
+        char[][] arr = {
+                {'A', 'A', 'A', 'A', 'A'},
+                {'A', ' ', ' ', ' ', 'A'},
+                {'A', 'A', 'A', 'A', 'A'},
+                {'A', ' ', ' ', ' ', 'A'},
+                {'A', ' ', ' ', ' ', 'A'},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+
+    public static char[][] letraB() {
+        char[][] arr = {
+                {'B', 'B', 'B', 'B', ' '},
+                {'B', ' ', ' ', ' ', 'B'},
+                {'B', 'B', 'B', 'B', ' '},
+                {'B', ' ', ' ', ' ', 'B'},
+                {'B', 'B', 'B', 'B', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+
+    public static char[][] letraC() {
+        char[][] arr = {
+                {' ', ' ', 'C', 'C', 'C'},
+                {' ', 'C', ' ', ' ', ' '},
+                {'C', ' ', ' ', ' ', ' '},
+                {' ', 'C', ' ', ' ', ' '},
+                {' ', ' ', 'C', 'C', 'C'},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+    public static char[][] letraD() {
+        char[][] arr = {
+                {'D', 'D', 'D', 'D', ' '},
+                {'D', ' ', ' ', ' ', 'D'},
+                {'D', ' ', ' ', ' ', 'D'},
+                {'D', ' ', ' ', ' ', 'D'},
+                {'D', 'D', 'D', 'D', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraE() {
+        char[][] arr = {
+                {'E', 'E', 'E', 'E', 'E'},
+                {'E', ' ', ' ', ' ', ' '},
+                {'E', 'E', 'E', 'E', 'E'},
+                {'E', ' ', ' ', ' ', ' '},
+                {'E', 'E', 'E', 'E', 'E'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraF() {
+        char[][] arr = {
+                {'F', 'F', 'F', 'F', 'F'},
+                {'F', ' ', ' ', ' ', ' '},
+                {'F', 'F', 'F', 'F', 'F'},
+                {'F', ' ', ' ', ' ', ' '},
+                {'F', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraG() {
+        char[][] arr = {
+                {'G', 'G', 'G', 'G', 'G'},
+                {'G', ' ', ' ', ' ', ' '},
+                {'G', 'G', 'G', 'G', 'G'},
+                {'G', ' ', ' ', ' ', 'G'},
+                {'G', 'G', 'G', 'G', 'G'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraH() {
+        char[][] arr = {
+                {'H', ' ', ' ', ' ', 'H'},
+                {'H', ' ', ' ', ' ', 'H'},
+                {'H', 'H', 'H', 'H', 'H'},
+                {'H', ' ', ' ', ' ', 'H'},
+                {'H', ' ', ' ', ' ', 'H'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraI() {
+        char[][] arr = {
+                {'I', 'I', 'I', 'I', 'I'},
+                {' ', ' ', 'I', ' ', ' '},
+                {' ', ' ', 'I', ' ', ' '},
+                {' ', ' ', 'I', ' ', ' '},
+                {'I', 'I', 'I', 'I', 'I'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraJ() {
+        char[][] arr = {
+                {'J', 'J', 'J', 'J', 'J'},
+                {' ', ' ', 'J', ' ', ' '},
+                {' ', ' ', 'J', ' ', ' '},
+                {' ', ' ', 'J', ' ', ' '},
+                {'J', 'J', 'J', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraK() {
+        char[][] arr = {
+                {'K', ' ', ' ', 'K', ' '},
+                {'K', ' ', 'K', ' ', ' '},
+                {'K', 'K', ' ', ' ', ' '},
+                {'K', ' ', 'K', ' ', ' '},
+                {'K', ' ', ' ', 'K', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraL() {
+        char[][] arr = {
+                {'L', ' ', ' ', ' ', ' '},
+                {'L', ' ', ' ', ' ', ' '},
+                {'L', ' ', ' ', ' ', ' '},
+                {'L', ' ', ' ', ' ', ' '},
+                {'L', 'L', 'L', 'L', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraM() {
+        char[][] arr = {
+                {'M', ' ', ' ', ' ', 'M'},
+                {'M', 'M', ' ', 'M', 'M'},
+                {'M', ' ', 'M', ' ', 'M'},
+                {'M', ' ', ' ', ' ', 'M'},
+                {'M', ' ', ' ', ' ', 'M'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraN() {
+        char[][] arr = {
+                {'N', ' ', ' ', ' ', 'N'},
+                {'N', 'N', ' ', ' ', 'N'},
+                {'N', ' ', 'N', ' ', 'N'},
+                {'N', ' ', ' ', 'N', 'N'},
+                {'N', ' ', ' ', ' ', 'N'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraO() {
+        char[][] arr = {
+                {'O', 'O', 'O', 'O', 'O'},
+                {'O', ' ', ' ', ' ', 'O'},
+                {'O', ' ', ' ', ' ', 'O'},
+                {'O', ' ', ' ', ' ', 'O'},
+                {'O', 'O', 'O', 'O', 'O'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraP() {
+        char[][] arr = {
+                {'P', 'P', 'P', 'P', 'P'},
+                {'P', ' ', ' ', ' ', 'P'},
+                {'P', 'P', 'P', 'P', 'P'},
+                {'P', ' ', ' ', ' ', ' '},
+                {'P', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraQ() {
+        char[][] arr = {
+                {' ', 'Q', 'Q', 'Q', ' '},
+                {'Q', ' ', ' ', ' ', 'Q'},
+                {'Q', ' ', ' ', ' ', 'Q'},
+                {' ', 'Q', 'Q', 'Q', ' '},
+                {' ', ' ', ' ', ' ', 'Q'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraR() {
+        char[][] arr = {
+                {'R', 'R', 'R', 'R', 'R'},
+                {'R', ' ', ' ', ' ', 'R'},
+                {'R', ' ', ' ', ' ', 'R'},
+                {'R', 'R', 'R', 'R', 'R'},
+                {'R', ' ', ' ', ' ', 'R'},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraS() {
+        char[][] arr = {
+                {' ', 'S', 'S', 'S', 'S'},
+                {' ', 'S', ' ', ' ', ' '},
+                {' ', ' ', 'S', ' ', ' '},
+                {' ', ' ', ' ', 'S', ' '},
+                {'S', 'S', 'S', 'S', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraT() {
+        char[][] arr = {
+                {'T', 'T', 'T', 'T', 'T'},
+                {' ', ' ', 'T', ' ', ' '},
+                {' ', ' ', 'T', ' ', ' '},
+                {' ', ' ', 'T', ' ', ' '},
+                {' ', ' ', 'T', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+
+        };
+        return arr;
+    }
+    public static char[][] letraU() {
+        char[][] arr = {
+                {'U', ' ', ' ', ' ', 'U'},
+                {'U', ' ', ' ', ' ', 'U'},
+                {'U', ' ', ' ', ' ', 'U'},
+                {'U', ' ', ' ', ' ', 'U'},
+                {'U', 'U', 'U', 'U', 'U'},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+    public static char[][] letraV() {
+        char[][] arr = {
+                {'V', ' ', ' ', ' ', 'V'},
+                {'V', ' ', ' ', ' ', 'V'},
+                {'V', ' ', ' ', ' ', 'V'},
+                {' ', 'V', ' ', 'V', ' '},
+                {' ', ' ', 'V', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+    public static char[][] letraW() {
+        char[][] arr = {
+                {'W', ' ', ' ', ' ', 'W'},
+                {'W', ' ', ' ', ' ', 'W'},
+                {'W', ' ', 'W', ' ', 'W'},
+                {'W', 'W', ' ', 'W', 'W'},
+                {'W', ' ', ' ', ' ', 'W'},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+    public static char[][] letraX() {
+        char[][] arr = {
+                {'X', ' ', ' ', ' ', 'X'},
+                {' ', 'X', ' ', 'X', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', 'X', ' ', 'X', ' '},
+                {'X', ' ', ' ', ' ', 'X'},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+    public static char[][] letraY() {
+        char[][] arr = {
+                {'Y', ' ', ' ', ' ', 'Y'},
+                {'Y', ' ', ' ', ' ', 'Y'},
+                {'Y', 'Y', 'Y', 'Y', 'Y'},
+                {' ', ' ', 'Y', ' ', ' '},
+                {' ', ' ', 'Y', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+    public static char[][] letraZ() {
+        char[][] arr = {
+                {'Z', 'Z', 'Z', 'Z', 'Z'},
+                {' ', ' ', ' ', 'Z', ' '},
+                {' ', ' ', 'Z', ' ', ' '},
+                {' ', 'Z', ' ', ' ', ' '},
+                {'Z', 'Z', 'Z', 'Z', 'Z'},
+                {' ', ' ', ' ', ' ', ' '}
+        };
+        return arr;
+    }
+    public static void escrever(char[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
